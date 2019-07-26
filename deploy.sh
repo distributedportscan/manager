ef4b26583dbac833a9ea432b6c9e5e16097b98e3
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2019-07-26 15:36:06 -0300

    add deploy function

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,16 @@ export WORKERPASS="$(cat src/etc/config.yaml | __yaml2json | jq -r '.rabbitmq | 
 
 envsubst < docker-compose.yml.sample > docker-compose.yml
 
-function start(){
+function build(){
+  rm -Rf oauth-proxy 2>/dev/null
+  git clone https://github.com/distributedportscan/oauth-proxy.git . && cd oauth-proxy
+  cd nginx && docker build -t nginx-authenticator . && cd ..
+  cd ..
+  rm -Rf oauth-proxy 2>/dev/null
   cd src && sudo docker build -t ds-manager . && cd ../
+}
+
+function start(){
   sudo docker-compose up -d
   sleep 5
   sudo docker exec -it rabbitmq rabbitmqctl add_user worker "$WORKERPASS"
